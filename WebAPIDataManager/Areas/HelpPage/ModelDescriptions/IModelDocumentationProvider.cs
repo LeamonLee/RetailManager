05c274f4e2ef3b6bd0b4f6450b2904b8d8c4c905
Leamon <leamon.lee13@gmail.com>
2020-02-06 10:54:32 +0800

    Added WebAPI Project

---
+++ WebAPIDataManager/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace WebAPIDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ WebAPIDataManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPIDataManager.WebApiApplication" Language="C#" %>
