91f5681e2f44d0380d9e0f61f978d6b32ebfca70
Leamon <leamon.lee13@gmail.com>
2020-02-06 20:12:43 +0800

    Added SQLDatabaseProject & WPFUIProject

---
+++ RMWpfUI/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using RMWpfUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMWpfUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //base.OnStartup(sender, e);
+
+            // OnStartup, launch ShellViewModel as our base view
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
