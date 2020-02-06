9f32a5952a08faa98071c8c69862e3304da739dd
Leamon <leamon.lee13@gmail.com>
2020-02-07 00:08:48 +0800

    Add Dependency Injection system in WPF

--- RMWpfUI/Bootstrapper.cs
+++ RMWpfUI/Bootstrapper.cs
@@ -11,9 +11,26 @@ namespace RMWpfUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _DIContainer = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _DIContainer.Instance(_DIContainer);
+
+            _DIContainer
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _DIContainer.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
@@ -22,6 +39,21 @@ namespace RMWpfUI
 
             // OnStartup, launch ShellViewModel as our base view
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _DIContainer.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _DIContainer.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _DIContainer.BuildUp(instance);
         }
     }
 }
--- WebAPIDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebAPIDataManager.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
